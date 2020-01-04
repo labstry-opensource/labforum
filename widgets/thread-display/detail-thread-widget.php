10c57ffff057ec27526923d4c53abb956597412a
littlephone <raynold_chow@hotmail.com>
2020-01-04 10:38:59 +0800

    Modulised Thread Display. Modulised thread display into widgets/thread-display folder
    Signed-off-by: littlephone <raynold_chow@hotmail.com>

---
+++ widgets/thread-display/detail-thread-widget.php
@@ -0,0 +1,20 @@
+<a class="thread-container col-12 d-block text-decoration-none"
+   href="/forum/thread.php?id={{:topic_id}}">
+    <div class="thread-slide container">
+        <div class="row py-2">
+            <div class="col-12 col-md-9 order-1 order-md-2">
+                <div class="thread-name">{{:topic_name}}</div>
+                <time class="timeago" datetime="{{:date}}">{{:date}}</time>
+                <div class="py-3 row">
+                    <div class="views col"><b>{{:views}}</b> views</div>
+                    <div class="replies col"><b>{{:number_of_replies}}</b> replies</div>
+
+                </div>
+            </div>
+            <div class="col-12 col-md-3 order-2 order-md-1 forum-name">
+                <div>{{:forum_name}}</div>
+                <div>{{:username}}</div>
+            </div>
+        </div>
+    </div>
+</a>
