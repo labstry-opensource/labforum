6a99bfa82a070bc39f401781fcecf29fef18114b
littlephone <raynold_chow@hotmail.com>
2020-01-23 21:45:28 +0800

    Minor bug fixes
    Signed-off-by: littlephone <raynold_chow@hotmail.com>

--- viewforum.php
+++ viewforum.php
@@ -1,14 +1,15 @@
 <?php
-
 require_once dirname(__FILE__) . '/laf-config.php';
 require_once LAF_PATH . "/classes/connect.php";
 require_once LAF_PATH . "/classes/UserRoles.php";
 require_once LAF_PATH . "/classes/Forum.php";
+include_once dirname(__FILE__) . '/maintenance.php' ;
 include_once dirname(__FILE__) . "/classes/Essentials.php";
 
 
 $roles = new UserRoles($pdoconnect);
 $roles->getUserRole(@$_SESSION['id']);
+
 
 if($roles->rights >= 90){
     include LAF_PATH . '/views/page-forum-unstable.php';
