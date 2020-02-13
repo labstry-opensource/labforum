c360e7120a4c4d1f7028a4acb13d8ed93de5f48e
littlephone <raynold_chow@hotmail.com>
2020-02-13 22:26:34 +0800

    Fixed bugs
    Signed-off-by: littlephone <raynold_chow@hotmail.com>

--- index.php
+++ index.php
@@ -3,14 +3,14 @@ require_once dirname(__FILE__) . '/laf-config.php';
 require_once dirname(__FILE__) . '/classes/connect.php';
 include_once dirname(__FILE__) . '/maintenance.php' ;
 include_once dirname(__FILE__) . '/classes/Users.php';
-include_once dirname(__FILE__) . '/classes/UserRoles.php';
+include_once dirname(__FILE__) . '/api/classes/UserRoles.php';
 include_once dirname(__FILE__) . '/classes/Sign.php';
 include_once dirname(__FILE__) .'/classes/Essentials.php';
 
 
 
 $roles = new UserRoles($pdoconnect);
-$roles->getUserRole(@$_SESSION['id']);
+$roles_arr = $roles->getUserRole(@$_SESSION['id']);
 
 
 include_once dirname(__FILE__) . '/views/page-index-stable.php';
