f8d0712a4fb388c3720255dba6e399dca94e910a
littlephone <raynold_chow@hotmail.com>
2020-11-17 23:08:27 +0000

    Fix check db connection not working due to calling non-existing laf-config

--- api/check-db-connection.php
+++ api/check-db-connection.php
@@ -2,8 +2,8 @@
 
 //Please define the configurations here. We have got no laf-config files.
 defined('LAF_PATH') || define('LAF_PATH', dirname($_SERVER['SCRIPT_FILENAME']));
-defined('LAF_ROOT_PATH') || define('LAF_ROOT_PATH', dirname(__FILE__));
-include LAF_ROOT_PATH . "/src/APITools.php";
+defined('INSTALL_API_ROOT_PATH') || define('INSTALL_API_ROOT_PATH', dirname(__FILE__));
+include INSTALL_API_ROOT_PATH . "../src/APITools.php";
 
 $apitools = new APITools();
 $lang = (isset($_GET['lang']) && checkTranslationExists($_GET['lang'], '/api/api-check-db-connection.php')) ? $_GET['lang'] : 'en';
