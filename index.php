e3e8d4967a7bb809e3d3b470017bc7ee147eb303
littlephone <raynold_chow@hotmail.com>
2020-11-18 13:27:19 +0000

    Fix minor bugs

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
 
 include_once dirname(__FILE__) . '/autoload.php';
-include_once LAF_ROOT_PATH . '/src/connect.php';
+include_once LAF_ROOT_PATH . '/src/Connect.php';
 
 
 $roles = new UserRoles($connection);
-$roles_arr = $roles->getUserRole(@$_SESSION['id']);
+$user_id = isset($_SESSION['id']) ? $_SESSION['id'] : 0;
+$roles_arr = $roles->getUserRole($user_id);
 
 
 include_once dirname(__FILE__) . '/views/page-index-stable.php';
